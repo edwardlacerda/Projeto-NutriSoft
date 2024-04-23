8791fc2141f15c1fb6de0f1d6e57c4e8fa9dcc15
Edward Lacerda <elacerda28@gmail.com>
2024-04-23 20:41:10 -0300

    commit do projeto NutriSoft para cálculo de IMC

---
+++ nutriSoft/src/nutrisoft/Main.java
@@ -0,0 +1,13 @@
+package nutrisoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TelaPrincipal tp = new TelaPrincipal();
+        tp.setVisible(true);
+        tp.setTitle("Nutrisoft - Menu Principal");
+        tp.setLocationRelativeTo(null);
+        tp.setResizable(false);
+    }
+
+}
